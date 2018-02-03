2903718c689343f5fa4d31ecb502c07322e0a57a
jcfasto <jcfausto@gmail.com>
2018-02-03 15:59:16 +0100

    Fixing CSRF validation issue.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   # Including the concerns so that they're available to controllers
   include Response
