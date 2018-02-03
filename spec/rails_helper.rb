27042c8203ab5feb7ad13c1e58e566530a0580e7
jcfasto <jcfausto@gmail.com>
2018-02-03 17:30:20 +0100

    Added SimpleCov for coverage reporting. 100% covered so far.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+
+# Simple Coverage gem initialization
+require 'simplecov'
+SimpleCov.start
+
 require 'database_cleaner'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
