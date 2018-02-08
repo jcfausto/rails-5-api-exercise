d2e723a8a80c454924d10ebc58a501851a645dbb
jcfasto <jcfausto@gmail.com>
2018-02-08 18:47:44 +0100

    ApplicationController specs. Tests failing.

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,30 @@
+# spec/controllers/application_controller.rb
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+  # create a test user
+  let(:user) { create(:user) }
+  # set headers
+  let(:headers) { { 'Authorization' => token_generator(user.id) } }
+  # set an invalid header
+  let(:invalid_headers) { { 'Authorization' => nil } }
+
+  describe '#authorize_request' do
+    context 'when auth token is passed' do
+      before { allow(request).to receive(:headers).and_return(headers) }
+
+      it 'sets the current user' do
+        expect(subject.instance_eval { authorize_request }).to eq(user)
+      end
+    end
+
+    context 'when auth token is not passed' do
+      before { allow(request).to receive(:headers).and_return(invalid_headers) }
+
+      it 'raises a MissingToken error' do
+        expect(subject.instance_eval { authorize_request }).to
+          raise_error(ExceptionHandler::MissingToken, /Missing token/)
+      end
+    end
+  end
+end
