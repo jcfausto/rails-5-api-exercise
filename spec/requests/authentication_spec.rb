b91c6f2b16b4aacd2568d19f149597adb31b3d6a
jcfasto <jcfausto@gmail.com>
2018-02-08 18:04:41 +0100

    Authentication controller specs. Tests failing

---
+++ spec/requests/authentication_spec.rb
@@ -0,0 +1,41 @@
+# spec/requests/authentication_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Authentication', type: :request do
+  describe 'POST /auth/login' do
+    # create a test user
+    let(:user) { create(:user) }
+    # set auth headers
+    let(:header) { valid_headers.except('Authorization') }
+    # set valid and invalid credentials
+    let(:valid_credentials) do
+      {
+        email: user.email,
+        password: user.password
+      }.to_json
+    end
+
+    let(:invalid_credentials) do
+      {
+        email: Faker::Internet.email,
+        password: Faker::Internet.password
+      }.to_json
+    end
+
+    context 'when request is valid' do
+        before { post '/auth/login', params: valid_credentials, headers: headers }
+
+        it 'returns an authentication token' do
+          expect(json['auth_token']).not_to be_nil
+        end
+    end
+
+    context 'when request is invalid' do
+      before { post '/auth/login', params: invalid_credentials, headers: headers }
+
+      it 'returns an invalid credentials message' do
+        expect(json['message']).to match(/Inevalid credentials/)
+      end
+    end
+  end
+end
