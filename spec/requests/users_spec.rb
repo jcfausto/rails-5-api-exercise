d33b045363fd4fdcea840bc9379b3c4dd3c77759
jcfasto <jcfausto@gmail.com>
2018-02-10 15:08:20 +0100

    User API spec changed to support account deletion via POST /account/delete. Tests failing

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -41,4 +41,30 @@ RSpec.describe 'Users API', type: :request do
     end
   end
 
+  describe 'POST /account/delete' do
+    context 'when request is valid' do
+      before { post '/account/delete', params: valid_attributes, headers: headers}
+
+      it 'destroy users\'s account' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns an account deleted success message' do
+        expect(json['message']).to match(/Account deleted/)
+      end
+    end
+
+    context 'when request is invalid' do
+      before { post '/account/delete', params: {}, headers: headers}
+
+      it 'does not delete the account' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a failure message' do
+        expect(json['message']).to match(/Validation failed/)
+      end
+
+    end
+  end
 end
